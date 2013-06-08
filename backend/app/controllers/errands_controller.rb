cad96e4ef12099fa36e36933a93d1938b2bf6168
Joan Magno <magnojoan@gmail.com>
2013-06-08 18:46:30 +0800

    Finished errand-making

--- backend/app/controllers/errands_controller.rb
+++ backend/app/controllers/errands_controller.rb
@@ -1,44 +1,38 @@
 class ErrandsController < ApplicationController
   respond_to :json
-
   def index
-     if env['warden'].authenticated?
-        render json: Errand.where(:user => current_user).all
-     else
-        render json: '', status: :unauthorized
-     end
+    #render json: Errand.where(:user => current_user).all
+    render json: Errand.all
   end
 
   def show
-     if env['warden'].authenticated?
-	render json: Errand.find(params[:id])
-     else
-        render json: '', status: :unauthorized
-     end
+    render json: Errand.find(params[:id])
   end
 
   def create
-     if env['warden'].authenticated?
-        errand = Errand.new
-        errand.title = params[:title]
-        errand.body = params[:body]
-        errand.deadline = params[:deadline]
-        errand.price = params[:price]
-        errand.user = current_user
-        errand.save!
-	render json: errand
-     else
-        render json: '', status: :unauthorized
-     end
+    errand = Errand.new
+    errand.title = params[:title]
+    errand.body = params[:body]
+    errand.deadline = params[:deadline]
+    errand.price = params[:price]
+    #errand.user = current_user
+    errand.save!
+    render json: errand
   end
 
   def update
-    env['warden'].authenticate!
-    render json: { :ok => true, :user => current_user.as_json }, status: :ok
+    errand = Errand.find(params[:id])
+    errand.title = params[:title]
+    errand.body = params[:body]
+    errand.deadline = params[:deadline]
+    errand.price = params[:price]
+    #errand.user = current_user
+    errand.save!
+    render json: errand
   end
 
   def destroy
-    env['warden'].logout
-    render json: { :ok => false, :message => "Logged out"}, status: :ok
+    errand = Errand.find(params[:id])
+    errand.delete!
   end
 end
