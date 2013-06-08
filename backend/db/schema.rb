a8b88397d8b9057d794f00c64ed02f288d851e91
kcvajgf <kevin.charles.atienza@gmail.com>
2013-06-08 19:01:22 +0800

    Added fbid column

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130608100728) do
+ActiveRecord::Schema.define(:version => 20130608105842) do
 
   create_table "errand_requests", :force => true do |t|
     t.integer  "errand_id"
@@ -30,6 +30,9 @@ ActiveRecord::Schema.define(:version => 20130608100728) do
     t.integer  "errand_request_id"
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
+    t.text     "location"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "ratings", :force => true do |t|
@@ -45,7 +48,7 @@ ActiveRecord::Schema.define(:version => 20130608100728) do
     t.string   "email"
     t.string   "password_digest"
     t.decimal  "credit"
-    t.text     "address"
+    t.text     "location"
     t.float    "latitude"
     t.float    "longitude"
     t.datetime "created_at",          :null => false
@@ -54,6 +57,7 @@ ActiveRecord::Schema.define(:version => 20130608100728) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "fb_id"
   end
 
 end
