be0f64a0a628a166a8a7b271771bc21d24c2167a
Tim Joseph Dumol <tim@timdumol.com>
2013-06-08 15:56:03 +0800

    Committing tim's changes.

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -1,2 +1,26 @@
 class SessionsController < ApplicationController
+  respond_to :json
+
+  def show
+    if env['warden'].authenticated?
+      render json: { ok: true, user: current_user.as_json }, status: :ok
+    else
+      # We'll actually never hit this place...
+      render json: { :ok => false }, status: :unauthorized
+    end
+  end
+
+  def update
+    env['warden'].authenticate!
+    render json: { :ok => true, :user => current_user.as_json }, status: :ok
+  end
+
+  def destroy
+    env['warden'].logout
+    render json: { :ok => false, :message => "Logged out"}, status: :ok
+  end
+
+  def unauthenticated()
+    render json: { :ok => false, :error  => (env['warden'].message || "Unauthenticated or unable to authenticate") }, status: :unauthorized
+  end
 end
