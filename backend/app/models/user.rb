e4f2f190e89c02495f2324ce62e04bd70958b910
kcvajgf <kevin.charles.atienza@gmail.com>
2013-06-08 18:29:35 +0800

    Fix typo

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :address, :credit, :email, :latitude, :longitude, :password_digest, :avatar
 
-  has_attached_file :avatar, :styles => { :medium => '300x300>', :thumb => '100x100' }, :default_url => '/images/:style/missing.png'
+  has_attached_file :avatar, :styles => { :medium => '300x300', :thumb => '100x100' }, :default_url => '/images/:style/missing.png'
  
   has_secure_password
 
