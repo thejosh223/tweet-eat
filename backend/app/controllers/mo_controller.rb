1a9ccb977a121312adae3dfa95ffb06c82185122
Reginald Eli A. Deinla <aldrineg@aldrineg-K55VJ.(none)>
2013-06-09 07:31:00 +0800

    Messaging shit.

--- backend/app/controllers/mo_controller.rb
+++ backend/app/controllers/mo_controller.rb
@@ -1,23 +1,46 @@
+require 'chikka'
+
 class MoController < ApplicationController
   def create
-    if params[:BODY].start_with?("VERIFY ")
-      user = User.where(:verification_code => params[:BODY].split(' ')[1]).first
-      user.trans_id = params[:TRANSID]
+    if params[:body].start_with?("VERIFY ")
+      user = User.where(:verification_code => params[:body].split(' ')[1]).first
+      user.trans_id = params[:transid]
+      user.phone_number = params[:msisdn]
+      #verified is verification_code.nil?
+      user.verification_code = nil
       user.save!
-    elsif params[:BODY].start_with?("COMPLETED ")
-      errand_request = ErrandRequest.find(params[:BODY].split(' ')[1])
+      sms(user.phone_number, user.trans_id, "Congratulations! Registration successful!", true)
+    elsif params[:body].start_with?("COMPLETED ")
+      errand_request = ErrandRequest.find(params[:body].split(' ')[1])
       errand_request.finished = true
       errand_request.save!
-    elsif params[:BODY].start_with?("YES ")
-      errand = Errand.find(params[:BODY].split(' ')[1])
+      errand = errand_request.errand
+      requestor = errand.user
+      runner = errand_request.user
+      sms(runner.phone_number, runner.trans_id, "#{requestor.first_name} #{requestor.last_name} is being notified that the task was completed.")
+      sms(requestor.phone_number, requestor.phone_number, "#{runner.first_name} #{runner.last_name} said he has finished the task. Is this true? Reply YES #{errand.id} or NO #{errand.id}")
+    elsif params[:body].start_with?("YES ")
+      errand = Errand.find(params[:body].split(' ')[1])
       errand.finished = true
       errand.save!
-    elsif params[:BODY].start_with?("NO ")
-      errand = Errand.find(params[:BODY].split(' ')[1])
+      errand_request = ErrandRequest.where(:errand_id => errand.id, :finished => true).first
+      requestor = errand.user
+      runner = errand_request.user
+      sms(runner.phone_number, runner.trans_id, "Congratulations, the payment will be transferred shortly.")
+      sms(requestor.phone_number, requestor.phone_number, "#{runner.first_name} #{runner.last_name} will receive the payment shortly.")
+    elsif params[:body].start_with?("NO ")
+      errand = Errand.find(params[:body].split(' ')[1])
       errand_request = ErrandRequest.where(:errand_id => errand.id, :finished => true).first
       errand_request.finished = false
       errand_request.save!
+      requestor = errand.user
+      runner = errand_request.user
+      sms(runner.phone_number, runner.trans_id, "Sorry but #{requestor.first_name} #{requestor.last_name} found your job unsatisfactory.")
+    else
+      sms(params[:msisdn], params[:transid], "Invalid keywords entered.")
+#      sms(requestor.phone_number, requestor.phone_number, "#{runner.first_name} #{runner.last_name} will receive the payment shortly.")
     end
     render json: {"STATUS" => "OK"}
   end
 end
+
