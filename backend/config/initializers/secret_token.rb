a1d73799a945eb3f4e67087b3716fb8424565e9c
Tim Joseph Dumol <tim@timdumol.com>
2013-06-08 13:13:37 +0800

    Added in the backend and frontend.

---
+++ backend/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Backend::Application.config.secret_token = '2cb9b8f20c0a3da37acfce89c5ed0beb9a1fdcc7e26e4b7ca3205432b730a4db44c0b6174658979e37be4adff148f270b64d8a762dc8d04a84c2d00e46d4c686'
