41106262d6e7930db0ba70bab00754f7cf134677
kcvajgf <kevin.charles.atienza@gmail.com>
2013-06-09 01:46:13 +0800

    Fixed polling data problems; added accept

--- backend/app/controllers/errand_requests_controller.rb
+++ backend/app/controllers/errand_requests_controller.rb
@@ -3,8 +3,19 @@ class ErrandRequestsController < ApplicationController
     requests = ErrandRequest.joins(:errand).where("not errands.finished AND errand_requests.user_id = ?", current_user.id).all
     render json: requests
   end
+  def update
+    request = ErrandRequest.find params[:id]
+    if not request.nil? and request.errand.user_id == current_user.id
+      errand = request.errand
+      errand.errand_request_id = request.id
+      errand.save!
+      render json: {ok: true}
+    else
+      render json: "", status: 404
+    end
+  end
   def pending
-    requests = ErrandRequest.joins(Errand.where("errand_request_id = ? AND user_id = ?", nil, current_user.id)).all
+    requests = ErrandRequest.joins(:errand).where("errand_request_id = ? AND errands.user_id = ?", nil, current_user.id).all
     render json: requests
   end
 end
