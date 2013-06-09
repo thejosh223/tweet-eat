9a258827cab2f14370efefddce099f5eaa40a81a
Reginald Eli A. Deinla <aldrineg@aldrineg-K55VJ.(none)>
2013-06-09 12:14:25 +0800

    git push

--- backend/app/controllers/mo_controller.rb
+++ backend/app/controllers/mo_controller.rb
@@ -10,7 +10,7 @@ class MoController < ApplicationController
       #verified is verification_code.nil?
       user.verification_code = nil
       user.save!
-      sms(user.phone_number, user.trans_id, "Congratulations! Registration successful!", true)
+      sms(user.phone_number, user.trans_id, "Congratulations registration successful", true)
     elsif params[:body].start_with?("COMPLETED ")
       errand_request = ErrandRequest.find(params[:body].split(' ')[1])
       errand_request.finished = true
@@ -27,7 +27,7 @@ class MoController < ApplicationController
       errand_request = ErrandRequest.where(:errand_id => errand.id, :finished => true).first
       requestor = errand.user
       runner = errand_request.user
-      sms(runner.phone_number, runner.trans_id, "Congratulations, the payment will be transferred shortly.")
+      sms(runner.phone_number, runner.trans_id, "Congratulations the payment will be transferred shortly")
       sms(requestor.phone_number, requestor.phone_number, "#{runner.first_name} #{runner.last_name} will receive the payment shortly.")
     elsif params[:body].start_with?("NO ")
       errand = Errand.find(params[:body].split(' ')[1])
@@ -37,6 +37,28 @@ class MoController < ApplicationController
       requestor = errand.user
       runner = errand_request.user
       sms(runner.phone_number, runner.trans_id, "Sorry but #{requestor.first_name} #{requestor.last_name} found your job unsatisfactory.")
+    elsif params[:body].start_with?("SEARCH ")
+      location = params[:body].split(' ', 2)[1]
+      found = Errand.near(location)
+      if (!found)
+        found = []
+      end
+
+      fs = found.collect { |e| "#{e.id} / #{e.title} / #{e.price}" }
+
+      message = fs.take(3).join("\n") + "\n Text GRAB <id> to bid for task."
+
+      puts message
+
+      sms(params[:msisdn], params[:transid], message, true)
+
+    elsif params[:body].start_with?("GRAB ")
+      user = User.where(:trans_id => params[:transid]).first
+      errand = Errand.find(params[:body].split(' ')[1])
+      if user and errand
+        ErrandRequest.new(:user_id => user.id, :errand_id => errand.id).save!
+        sms(user.phone_number, user.trans_id, "You have submitted a bid!")
+      end
     else
       puts 'sana di nageerrorrrrrrr'
       sms(params[:msisdn], params[:transid], "Invalid keywords entered.", true)
--- backend/lib/chikka.rb
+++ backend/lib/chikka.rb
@@ -107,3 +107,4 @@ def test()
 
   puts encoded_signature
 end
+
