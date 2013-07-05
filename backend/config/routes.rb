5a60f061f25a39a05b0150218866289039eb6813
Joan Magno <magnojoan@gmail.com>
2013-07-05 23:00:57 +0800

    Root page now appears but looks like shit

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,8 @@
 Backend::Application.routes.draw do
   root :to => "restaurants#index"
-  resources :restaurants
+  resources :restaurants do
+    collection do
+      get 'search'
+    end
+  end
 end
